d0c500a9f66f12efac4b2038d5ffe019ce1ef1fd
Alvee Akand <AlveeM@users.noreply.github.com>
2019-09-29 11:48:17 +0600

    reverse string

--- data-structures/arrays/string-reverse.js
+++ data-structures/arrays/string-reverse.js
@@ -1,7 +1,34 @@
-// Craeting a function that reverses a string:
-// 'Hi My name is Andrei' should be:
-// 'ierdnA si eman yM iH'
+// Creating a function that reverses a string:
+// 'Hi My name is Alvee' should be:
+// 'eevlA si eman yM iH'
 
 function reverse(str) {
+    // chech input
+    if (!str || str.length < 2 || typeof str !== 'string') {
+        return 'please provide a valid input';
+    }
 
+    const reversed = [];
+    const totalItems = str.length - 1;
+    for (let i = totalItems; i >= 0; i--) {
+        reversed.push(str[i]);
+    }
+
+    return reversed.join('');
 }
+
+console.log('reverse 1', reverse('Hi My name is Alvee'));
+
+
+// Method 2
+function reverse2(str) {
+    return str.split('').reverse().join('');
+}
+
+console.log('reverse 2', reverse2('Hi My name is Alvee'));
+
+
+// Method 3
+const reverse3 = str=> [...str].reverse().join('');
+
+console.log('reverse 3', reverse3('Hi My name is Alvee'));
