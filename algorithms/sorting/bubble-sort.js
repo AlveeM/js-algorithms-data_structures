5f7f82dae8a2abb26b3660b1b85dccee0c6e71f8
Alvee Akand <AlveeM@users.noreply.github.com>
2019-10-02 19:16:27 +0600

    added selection sort

--- algorithms/sorting/bubble-sort.js
+++ algorithms/sorting/bubble-sort.js
@@ -2,15 +2,15 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 // TC: O(n^2)
 // SC: 1
-function bubbleSort(array) {
-    const length = array.length;
+function bubbleSort(arr) {
+    const length = arr.length;
     for (let i=0; i < length; i++) {
         for (let j=0; j < length; j++) {
-            if (array[j] > array[j+1]) {
+            if (arr[j] > arr[j+1]) {
                 // Swap
-                let temp = array[j]
-                array[j] = array[j+1];
-                array[j+1] = temp;
+                let temp = arr[j]
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
