fd007b9f8a3b55a74d51e2038802df3bd53ba327
Alvee Akand <AlveeM@users.noreply.github.com>
2019-10-03 22:21:40 +0600

    added breadthFirstSearch and depthFirstSearch

---
+++ algorithms/searching-traversal/playground.js
@@ -0,0 +1,15 @@
+var beasts = ['Centaur', 'Godzilla', 'Mosura', 'Minotaur', 'Hydra', 'Nessie'];
+
+console.log(beasts.indexOf('Godzilla'));
+
+console.log(beasts.findIndex(function(item){
+    return item === 'Godzilla';
+}))
+
+console.log(beasts.find(function(item) {
+    return item === 'Godzilla';
+}))
+
+console.log(beasts.includes('Godzilla'));
+
+// all of the above function uses linear search
