1ad3a3fd227992836ac3d2a26860a3bb19f171f5
Alvee Akand <AlveeM@users.noreply.github.com>
2020-01-11 18:36:56 -0500

    added queue implementation 02

---
+++ data-structures/stacks-and-queues/queue-implementation-02.js
@@ -0,0 +1,50 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueue(val) {
+        let newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+
+        this.size++;
+        return this.size;
+    }
+
+    dequeue() {
+        if (!this.first) {
+            return null;
+        }
+
+        let temp = this.first;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+
+        this.first = this.first.next;
+
+        this.size--;
+        return temp.value;
+    }
+}
+
+let queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
