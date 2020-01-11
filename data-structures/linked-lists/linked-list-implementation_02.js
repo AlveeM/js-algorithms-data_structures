5b72411e43654c73598c7973898af5717c22fb14
Alvee Akand <AlveeM@users.noreply.github.com>
2020-01-11 12:03:22 -0500

    added insert() to linked list implementation 02

--- data-structures/linked-lists/linked-list-implementation_02.js
+++ data-structures/linked-lists/linked-list-implementation_02.js
@@ -92,6 +92,27 @@ class SinglyLinkedList {
         return false;
     }
 
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === this.length) {
+            this.push(val);
+            return true;
+        }
+        if (index === 0) {
+            this.unshift(val);
+            return true;
+        }
+
+        let newNode = new Node(val);
+        let prev = this.get(index - 1);
+        let temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+
+        return true;
+    }
+
     traverse() {
         let current = this.head;
         while (current) {
