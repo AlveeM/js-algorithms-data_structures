217ddd87e75ed81147bc3a44183f8add9bb6331b
Alvee Akand <AlveeM@users.noreply.github.com>
2020-01-11 12:27:01 -0500

    added reverse() to linked list implementation 02

--- data-structures/linked-lists/linked-list-implementation_02.js
+++ data-structures/linked-lists/linked-list-implementation_02.js
@@ -129,6 +129,27 @@ class SinglyLinkedList {
         return removed;
     }
 
+    reverse() {
+        // swap head and tail
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+
+        // create storage variables
+        let next;
+        let prev = null;
+
+        // iterate and switch references
+        for (let i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+
+        return this;
+    }
+
     traverse() {
         let current = this.head;
         while (current) {
