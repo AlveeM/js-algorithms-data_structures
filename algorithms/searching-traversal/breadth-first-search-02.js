d1226398055abd78ca6e43fac618bdb158117177
Alvee Akand <AlveeM@users.noreply.github.com>
2020-01-11 21:14:34 -0500

    added breadth-first search 02

---
+++ algorithms/searching-traversal/breadth-first-search-02.js
@@ -0,0 +1,102 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        let newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while(true) {
+                if (value === current.value) {
+                    return undefined;
+                }
+
+                if (value < current.value) {
+                    if(current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (value > current.value) {
+                    if(current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+
+    find(value) {
+        if (this.root === null) {
+            return false;
+        }
+
+        let current = this.root;
+        let found = false;
+
+        while(current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+
+        if (!found) {
+            return undefined;
+        } else {
+            return current;
+        }
+    }
+
+    BFS() {
+        let node;
+        let data = [];
+        let queue = [];
+        queue.push(this.root);
+
+        while(queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if (node.left) {
+                queue.push(node.left);
+            }
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+
+        return data;
+    }
+
+}
+
+//          10
+//      5       13
+//   2    7   11  16
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
