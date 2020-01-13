1fda18cee8bd6fb32bdd3159ac09b1a33c0cac7d
Alvee Akand <AlveeM@users.noreply.github.com>
2020-01-13 18:28:40 -0500

    added breadth first traversal

---
+++ data-structures/graphs/breadth-first-traversal.js
@@ -0,0 +1,74 @@
+class Graph {
+    constructor() {
+      this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+      if (!this.adjacencyList[vertex]) {
+        this.adjacencyList[vertex] = [];
+      }
+      return undefined;
+    }
+
+    removeVertex (vertex) {
+      while(this.adjacencyList[vertex].length) {
+        const adjacencyVertex = this.adjacencyList[vertex].pop();
+        this.removeEdge(vertex, adjacencyVertex);
+      }
+      delete this.adjacencyList[vertex];
+    }
+
+    addEdge(v1, v2) {
+      this.adjacencyList[v1].push(v2);
+      this.adjacencyList[v2].push(v1);
+    }
+
+    removeEdge (v1, v2) {
+      this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+        vertex => vertex !== v2
+      );
+      this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+        vertex => vertex !== v1
+      );
+    }
+
+    breadthFirstTraversal(start) {
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+        visited[start] = true;
+
+        while(queue.length) {
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            })
+        }
+
+        return result;
+    }
+
+  }
+
+let g = new Graph();
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A","B")
+g.addEdge("A","C")
+g.addEdge("B","D")
+g.addEdge("C","E")
+g.addEdge("D","E")
+g.addEdge("D","F")
+g.addEdge("E","F")
