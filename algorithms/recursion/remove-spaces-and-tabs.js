02e954b717cd93811270c4db66f24475c400b13d
Alvee Akand <AlveeM@users.noreply.github.com>
2020-02-09 11:06:10 -0500

    removed debugger statement in remove-spaces-and-tabs

--- algorithms/recursion/remove-spaces-and-tabs.js
+++ algorithms/recursion/remove-spaces-and-tabs.js
@@ -2,7 +2,6 @@ const remove = (string) => {
     if (string.length === 0) {
         return "";
     }
-    debugger;
     if (string[0] === "\t" || string[0] === " ") {
         return remove(string.substring(1));
     } else {
